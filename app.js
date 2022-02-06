b8bbfbab2cffa35bb3089c994652e2ad53eae475
Havronskyi Nazarii <nazar.gavronsky.mail@gmail.com>
2022-02-06 23:48:29 +0200

    mongoose add

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
+const mongoose = require('mongoose');
+
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/newStart');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -9,8 +14,6 @@ const userRouter = require('./routes/user.router');
 app.use('/users', userRouter);
 
 
-
-
 app.listen(5100, ( () => {
-    console.log('app listen 5100')
+    console.log('app listen 5100');
 } ));
